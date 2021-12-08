8573e4064bf3293de317c68d39f7e3c5bdc7c71c
huchi57 <felixhu0312@gmail.com>
2021-12-09 02:24:28 +0800

    Update LightFlicker.cs

--- LightFlicker/LightFlicker.cs
+++ LightFlicker/LightFlicker.cs
@@ -1,54 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
 
 [RequireComponent(typeof(Light))]
 public class LightFlicker : MonoBehaviour
 {
-    private static readonly string _none = "a";
-    private static readonly string _normal = "m";
-    private static readonly string _doubleBrightness = "z";
-    private static readonly string _fluorescentFlicker = "mmamammmmammamamaaamammma";
-    private static readonly string _slowStrobe = "aaaaaaaazzzzzzzz";
-    private static readonly string _gentlePulse = "jklmnopqrstuvwxyzyxwvutsrqponmlkj";
-    private static readonly string _slowStrongPulse = "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba";
-    private static readonly string _slowPulseNotFadeToBlack = "abcdefghijklmnopqrrqponmlkjihgfedcba";
-    private static readonly string _fastStrobe = "mamamamamama";
-    private static readonly string _underwaterLightMutation = "mmnnmmnnnmmnn";
-    private static readonly string _candle1 = "mmmmmaaaaammmmmaaaaaabcdefgabcdefg";
-    private static readonly string _candle2 = "mmmaaaabcdefgmmmmaaaammmaamm";
-    private static readonly string _candle3 = "mmmaaammmaaammmabcdefaaaammmmabcdefmmmaaaa";
-    private static readonly string _flicker1 = "mmnmmommommnonmmonqnmmo";
-    private static readonly string _flicker2 = "nmonqnmomnmomomno";
-
-    public enum Preset
-    { 
-        None,
-        Normal,
-        DoubleBrightness,
-        FluorescentFlicker,
-        SlowStrobe,
-        GentlePulse,
-        SlowStrongPulse,
-        SlowPulseNotFadeToBlack,
-        FastStrobe,
-        UnderwaterLightMutation,
-        [InspectorName("Candle (First Variety)")]   Candle1,
-        [InspectorName("Candle (Second Variety)")]  Candle2,
-        [InspectorName("Candle (Third Variety)")]   Candle3,
-        [InspectorName("Flicker (First Variety)")]  Flicker1,
-        [InspectorName("Flicker (Second Variety)")] Flicker2,
-        Custom
-    }
-
-    [SerializeField] private string _pattern = _normal;
-    [SerializeField] private Preset _preset = Preset.Normal;
+    //[Header("Flicker Pattern (a: 0% / m: 100% / z: 200% Normal Brightness)")]
+    [SerializeField] private string _pattern = "m";
+    [SerializeField] private LightFlickerUtility.Preset _preset = LightFlickerUtility.Preset.Normal;
     [SerializeField] private float _normalBrightness = 1;
+
+    //[Header("Speed Settings")]
+    [SerializeField] private float _patternDuration = 1;
+    [SerializeField] private bool _lerp = true;
+    [SerializeField] private float _lerpSpeed = 10;
 
     private Light _light = default;
     private string _cachedPattern = default;
-    private Preset _cachedPreset = default;
+    private LightFlickerUtility.Preset _cachedPreset = default;
+    private int _index = 0;
+    private float _timer = 0;
+    private float _targetIntensity = 0;
 
     public string Pattern 
     { 
@@ -59,73 +30,68 @@ public class LightFlicker : MonoBehaviour
         } 
     }
 
-    public Preset PresetPattern 
+    public LightFlickerUtility.Preset PresetPattern 
     { 
         get => _preset; 
         set 
         { 
             _preset = value; 
-            _pattern = GetPatterFromPreset(_preset); 
+            _pattern = LightFlickerUtility.GetPatterFromPreset(_preset, _pattern); 
         } 
     }
 
     public float NormalBrightness { get => _normalBrightness; set => _normalBrightness = value; }
-
-    private string GetPatterFromPreset(Preset preset)
-    {
-        switch (preset)
-        {
-            case Preset.None: return _none;
-            case Preset.Normal: return _normal;
-            case Preset.DoubleBrightness: return _doubleBrightness;
-            case Preset.FluorescentFlicker: return _fluorescentFlicker;
-            case Preset.SlowStrobe: return _slowStrobe;
-            case Preset.GentlePulse: return _gentlePulse;
-            case Preset.SlowStrongPulse: return _slowStrongPulse;
-            case Preset.SlowPulseNotFadeToBlack: return _slowPulseNotFadeToBlack;
-            case Preset.FastStrobe: return _fastStrobe;
-            case Preset.UnderwaterLightMutation: return _underwaterLightMutation;
-            case Preset.Candle1: return _candle1;
-            case Preset.Candle2: return _candle2;
-            case Preset.Candle3: return _candle3;
-            case Preset.Flicker1: return _flicker1;
-            case Preset.Flicker2: return _flicker2;
-            case Preset.Custom: return _pattern;
-            default: break;
-        }
-        return _pattern;
-    }
+    public float PatternDuration { get => _patternDuration; set => _patternDuration = value; }
+    public bool Lerp { get => _lerp; set => _lerp = value; }
+    public float LerpSpeed { get => _lerpSpeed; set => _lerpSpeed = value < 0 ? 0 : value; }
 
     private string RestrictToLowercaseAlphabets(string input)
     {
-        // Lower alphabets ASCII ranged from 97 too 122
+        // Lower alphabets, i.e. 'a'-'z', have ASCII values ranged from 97 to 122 (In hex: from 61 to 7A)
         return Regex.Replace(input, @"[^\u0061-\u007A]+", string.Empty);
+    }
+    
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _patternDuration)
+        {
+            _timer = 0;
+        }
+
+        _index = (int)((_timer / _patternDuration) * _pattern.Length - 1);
+        _index = Mathf.Clamp(_index, 0, _pattern.Length - 1);
+        _targetIntensity = (_pattern[_index] - 'a') / 12.5f * _normalBrightness;
+        _light.intensity = _lerp ? Mathf.Lerp(_light.intensity, _targetIntensity, _lerpSpeed * Time.deltaTime) : _targetIntensity;
     }
 
     private void Awake()
     {
         _light = GetComponent<Light>();
-    }
-
-    private void Update()
-    {
-        
+        _index = Random.Range(0, _pattern.Length);
     }
 
     private void OnValidate()
     {
         _pattern = RestrictToLowercaseAlphabets(_pattern);
+
         if (_cachedPreset != _preset)
         {
             _cachedPreset = _preset;
-            _pattern = GetPatterFromPreset(_preset);
+            _pattern = LightFlickerUtility.GetPatterFromPreset(_preset, _pattern);
             _cachedPattern = _pattern;
         }
+
         if (_cachedPattern != _pattern)
         {
             _cachedPattern = _pattern;
-            _preset = Preset.Custom;
+            _preset = LightFlickerUtility.Preset.Custom;
             _cachedPreset = _preset;
+        }
+
+        if (_lerpSpeed < 0)
+        {
+            _lerpSpeed = 0;
         }
     }
 
