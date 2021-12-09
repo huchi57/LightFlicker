8c72139f1c453f79a267c8c42fde7f955562b5cd
huchi57 <felixhu0312@gmail.com>
2021-12-09 16:09:04 +0800

    Update LightFlickerEditor.cs

--- LightFlicker/Editor/LightFlickerEditor.cs
+++ LightFlicker/Editor/LightFlickerEditor.cs
@@ -1,5 +1,3 @@
-﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -7,14 +5,19 @@ using UnityEditor;
 [CustomEditor(typeof(LightFlicker))]
 public class LightFlickerEditor : Editor
 {
+    private const float _graphHeight = 90f;
+
     private SerializedProperty _pattern = default;
     private SerializedProperty _preset = default;
     private SerializedProperty _normalBrightness = default;
     private SerializedProperty _patternDuration = default;
     private SerializedProperty _lerp = default;
     private SerializedProperty _lerpSpeed = default;
+
     private GUIStyle _cacheStyle = default;
     private GUIStyle _style = default;
+    private AnimationCurve _patternCurve = default;
+    private Rect _graphRect = default;
 
     private void Awake()
     {
@@ -24,6 +27,7 @@ public class LightFlickerEditor : Editor
         _patternDuration = serializedObject.FindProperty(nameof(_patternDuration));
         _lerp = serializedObject.FindProperty(nameof(_lerp));
         _lerpSpeed = serializedObject.FindProperty(nameof(_lerpSpeed));
+        _patternCurve = new AnimationCurve();
     }
 
     public override void OnInspectorGUI()
@@ -32,25 +36,75 @@ public class LightFlickerEditor : Editor
         _cacheStyle = GUI.skin.label;
         _style = _cacheStyle;
 
-        GUILayout.BeginHorizontal();
-        GUILayout.BeginVertical();
-        _style.alignment = TextAnchor.MiddleLeft;
-        GUILayout.Label("Alphabet [ a -", _style);
-        GUILayout.Label("Strength [ 0%", _style);
-        GUILayout.EndVertical();
-        GUILayout.BeginVertical();
-        _style.alignment = TextAnchor.MiddleCenter;
-        GUILayout.Label("- m -", _style);
-        GUILayout.Label("100%", _style);
-        GUILayout.EndVertical();
-        GUILayout.BeginVertical();
-        _style.alignment = TextAnchor.MiddleRight;
-        GUILayout.Label("- z ]", _style);
-        GUILayout.Label("200% ]", _style);
-        GUILayout.EndVertical();
-        GUILayout.EndHorizontal();
-
+        DrawPreviewGraph();
+        DrawHeaderInfo();
         GUILayout.Space(10);
+        DrawProperties();
+
+        GUI.skin.label = _cacheStyle;
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private Keyframe[] GetKeyframes()
+    {
+        var keyframes = new List<Keyframe>();
+        for (int i = 0; i < _pattern.arraySize; i++)
+        {
+            keyframes.Add
+            (
+                new Keyframe 
+                { 
+                    time = i, 
+                    value = (_pattern.stringValue[i] - 'a') / 12.5f
+                }
+            );
+        }
+
+        // "Magic Keyframe" for array size = 1
+        if (_pattern.arraySize <= 1)
+        {
+            keyframes.Add(new Keyframe { time = 1, value = keyframes[0].value });
+        }
+        return keyframes.ToArray();
+    }
+
+    private void DrawPreviewGraph()
+    {
+        GUILayout.Label("Flickering Pattern Preview", EditorStyles.boldLabel);
+        using (var scope = new GUILayout.HorizontalScope(GUILayout.Height(_graphHeight)))
+        {
+            using (var scope2 = new GUILayout.VerticalScope(GUILayout.Width(40)))
+            {
+                _style.alignment = TextAnchor.MiddleLeft;
+                GUILayout.Label("200%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
+                GUILayout.Label("100%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
+                GUILayout.Label("0%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
+            }
+
+            GUI.enabled = false;
+            _patternCurve.keys = GetKeyframes();
+            EditorGUILayout.CurveField("", _patternCurve, Color.yellow, new Rect(0, 0, _pattern.arraySize - 1, 2), GUILayout.ExpandWidth(true), GUILayout.Height(_graphHeight));
+            GUI.enabled = true;
+        }
+    }
+
+    private void DrawHeaderInfo()
+    {
+        GUILayout.BeginHorizontal();
+        using (var scope = new GUILayout.VerticalScope())
+        {
+            _style.fontStyle = FontStyle.Italic;
+            _style.alignment = TextAnchor.MiddleCenter;
+            GUILayout.Label("Normal Brightness scale from a to z:", _style);
+            GUILayout.Label("a - 0%, m - 100%, z - 200%", _style);
+            _style.fontStyle = FontStyle.Normal;
+            _style.alignment = TextAnchor.MiddleLeft;
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    private void DrawProperties()
+    {
         GUILayout.Label("Flicker Pattern", EditorStyles.boldLabel);
         EditorGUILayout.PropertyField(_pattern);
         EditorGUILayout.PropertyField(_preset);
@@ -61,8 +115,5 @@ public class LightFlickerEditor : Editor
         EditorGUILayout.PropertyField(_patternDuration);
         EditorGUILayout.PropertyField(_lerp);
         EditorGUILayout.PropertyField(_lerpSpeed);
-
-        GUI.skin.label = _cacheStyle;
-        serializedObject.ApplyModifiedProperties();
     }
 }
