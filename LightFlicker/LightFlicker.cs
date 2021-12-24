132649d7caef05d8ff54d2c61c698a52bf2e722a
huchi57 <felixhu0312@gmail.com>
2021-12-25 01:01:15 +0800

    Update LightFlicker.cs

--- LightFlicker/LightFlicker.cs
+++ LightFlicker/LightFlicker.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Text.RegularExpressions;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(Light))]
 public class LightFlicker : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class LightFlicker : MonoBehaviour
     [SerializeField] private float _patternDuration = 1;
     [SerializeField] private bool _lerp = true;
     [SerializeField] private float _lerpSpeed = 10;
+
+    [SerializeField] [HideInInspector] private bool _playingInEditMode = false;
 
     private Light _light = default;
     private string _cachedPattern = default;
@@ -40,19 +43,46 @@ public class LightFlicker : MonoBehaviour
         } 
     }
 
+    private Light Light
+    {
+        get
+        {
+            if (_light == null)
+            {
+                _light = GetComponent<Light>();
+            }
+            return _light;
+        }
+    }
+
     public float NormalBrightness { get => _normalBrightness; set => _normalBrightness = value; }
     public float PatternDuration { get => _patternDuration; set => _patternDuration = value; }
     public bool Lerp { get => _lerp; set => _lerp = value; }
     public float LerpSpeed { get => _lerpSpeed; set => _lerpSpeed = value < 0 ? 0 : value; }
+    public float Progress => ((_timer / _patternDuration) * _pattern.Length - 1) / _pattern.Length;
 
     private string RestrictToLowercaseAlphabets(string input)
     {
         // Lower alphabets, i.e. 'a'-'z', have ASCII values ranged from 97 to 122 (In hex: from 61 to 7A)
         return Regex.Replace(input, @"[^\u0061-\u007A]+", string.Empty);
     }
-    
+
     private void Update()
     {
+
+#if UNITY_EDITOR
+        if (!UnityEditor.EditorApplication.isPlaying && !_playingInEditMode)
+        {
+            Reset();
+            return;
+        }
+
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            _playingInEditMode = false;
+        }
+#endif
+
         _timer += Time.deltaTime;
         if (_timer > _patternDuration)
         {
@@ -62,18 +92,27 @@ public class LightFlicker : MonoBehaviour
         _index = (int)((_timer / _patternDuration) * _pattern.Length - 1);
         _index = Mathf.Clamp(_index, 0, _pattern.Length - 1);
         _targetIntensity = (_pattern[_index] - 'a') / 12.5f * _normalBrightness;
-        _light.intensity = _lerp ? Mathf.Lerp(_light.intensity, _targetIntensity, _lerpSpeed * Time.deltaTime) : _targetIntensity;
+        Light.intensity = _lerp ? Mathf.Lerp(Light.intensity, _targetIntensity, _lerpSpeed * Time.deltaTime) : _targetIntensity;
     }
 
     private void Awake()
     {
-        _light = GetComponent<Light>();
         _index = Random.Range(0, _pattern.Length);
     }
 
     private void OnValidate()
     {
         _pattern = RestrictToLowercaseAlphabets(_pattern);
+
+        if (_normalBrightness < 0)
+        {
+            _normalBrightness = 0;
+        }
+
+        if (_patternDuration < 0.001f)
+        {
+            _patternDuration = 0.001f;
+        }
 
         if (_cachedPreset != _preset)
         {
@@ -93,11 +132,27 @@ public class LightFlicker : MonoBehaviour
         {
             _lerpSpeed = 0;
         }
+
+        Light.intensity = _normalBrightness;
     }
+
+#if UNITY_EDITOR
+    private void OnEnable()
+    {
+        UnityEditor.EditorApplication.update += Update;
+        Reset();
+    }
+
+    private void OnDisable()
+    {
+        UnityEditor.EditorApplication.update -= Update;
+        Reset();
+    }
+#endif
 
     private void Reset()
     {
-        _light = GetComponent<Light>();
-        _normalBrightness = _light.intensity;
+        Light.intensity = _normalBrightness;
+        _playingInEditMode = false;
     }
 }
