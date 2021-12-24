65510e9b658f73889369015bc144119d17e3872f
huchi57 <felixhu0312@gmail.com>
2021-12-25 01:36:13 +0800

    Update LightFlickerEditor.cs

--- LightFlicker/Editor/LightFlickerEditor.cs
+++ LightFlicker/Editor/LightFlickerEditor.cs
@@ -1,181 +1,157 @@
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEditor;
 
-[ExecuteInEditMode]
-[RequireComponent(typeof(Light))]
-public class LightFlicker : MonoBehaviour
+[CustomEditor(typeof(LightFlicker))]
+public class LightFlickerEditor : Editor
 {
-    // [Header("Flicker Pattern (a: 0% / m: 100% / z: 200% Normal Brightness)")]
-    [SerializeField] private string _pattern = "m";
-    [SerializeField] private LightFlickerUtility.Preset _preset = LightFlickerUtility.Preset.Normal;
-    [SerializeField] private float _normalBrightness = 1;
+    private const float _graphHeight = 90f;
 
-    // [Header("Speed Settings")]
-    [SerializeField] private float _patternDuration = 1;
-    [SerializeField] private bool _lerp = true;
+    private SerializedProperty _pattern = default;
+    private SerializedProperty _preset = default;
+    private SerializedProperty _normalBrightness = default;
+    private SerializedProperty _patternDuration = default;
+    private SerializedProperty _lerp = default;
+    private SerializedProperty _playingInEditMode = default;
+    private SerializedProperty _intensityCurve = default;
 
-    // Serialized for custom editor
-    [SerializeField] [HideInInspector] private bool _playingInEditMode = false;
-    [SerializeField] [HideInInspector] private AnimationCurve _intensityCurve = new AnimationCurve();
+    private static Texture2D _red = null;
+    private GUIStyle _cacheStyle = default;
+    private GUIStyle _style = default;
 
-    // Internal variables
-    private Light _light = default;
-    private string _cachedPattern = default;
-    private LightFlickerUtility.Preset _cachedPreset = default;
-    private float _timer = 0;
+    private LightFlicker Target => (LightFlicker) target;
+    private SerializedProperty Pattern { get => GetPropertyByName(_pattern, nameof(_pattern)); }
+    private SerializedProperty Preset { get => GetPropertyByName(_preset, nameof(_preset)); }
+    private SerializedProperty NormalBrightness { get => GetPropertyByName(_normalBrightness, nameof(_normalBrightness)); }
+    private SerializedProperty PatternDuration { get => GetPropertyByName(_patternDuration, nameof(_patternDuration)); }
+    private SerializedProperty Lerp { get => GetPropertyByName(_lerp, nameof(_lerp)); }
+    private SerializedProperty PlayingInEditMode { get => GetPropertyByName(_playingInEditMode, nameof(_playingInEditMode)); }
+    private SerializedProperty IntensityCurve { get => GetPropertyByName(_intensityCurve, nameof(_intensityCurve)); }
 
-    public string Pattern 
-    { 
-        get => _pattern; 
-        set 
-        { 
-            _pattern = RestrictToLowercaseAlphabets(value); 
-        } 
-    }
-
-    public LightFlickerUtility.Preset PresetPattern 
-    { 
-        get => _preset; 
-        set 
-        { 
-            _preset = value; 
-            _pattern = LightFlickerUtility.GetPatterFromPreset(_preset, _pattern); 
-        } 
-    }
-
-    private Light Light
+    private static Texture2D Red
     {
         get
         {
-            if (_light == null)
+            if (_red == null)
             {
-                _light = GetComponent<Light>();
+                _red = new Texture2D(1, 1);
+                _red.SetPixel(0, 0, Color.red);
+                _red.Apply();
             }
-            return _light;
+            return _red;
         }
     }
 
-    public float NormalBrightness { get => _normalBrightness; set => _normalBrightness = value; }
-    public float PatternDuration { get => _patternDuration; set => _patternDuration = value; }
-    public bool Lerp { get => _lerp; set => _lerp = value; }
-    public float Progress => ((_timer / _patternDuration) * _pattern.Length - 1) / _pattern.Length;
-
-    private string RestrictToLowercaseAlphabets(string input)
+    public override void OnInspectorGUI()
     {
-        // Lower alphabets, i.e. 'a'-'z', have ASCII values ranged from 97 to 122 (In hex: from 61 to 7A)
-        return Regex.Replace(input, @"[^\u0061-\u007A]+", string.Empty);
+        Repaint();
+        serializedObject.Update();
+        _cacheStyle = GUI.skin.label;
+        _style = _cacheStyle;
+
+        DrawPreviewGraph();
+        DrawHeaderInfo();
+        GUILayout.Space(10);
+        DrawProperties();
+
+        GUI.skin.label = _cacheStyle;
+        serializedObject.ApplyModifiedProperties();
     }
 
-    private void Update()
+    private SerializedProperty GetPropertyByName(SerializedProperty property, string name)
     {
-
-#if UNITY_EDITOR
-        if (!UnityEditor.EditorApplication.isPlaying && !_playingInEditMode)
+        if (property == null)
         {
-            Reset();
-            return;
+            property = serializedObject.FindProperty(name);
         }
-
-        if (UnityEditor.EditorApplication.isPlaying)
-        {
-            _playingInEditMode = false;
-        }
-#endif
-
-        _timer += Time.deltaTime;
-        if (_timer > _patternDuration)
-        {
-            _timer = 0;
-        }
-
-        var time = (_timer / _patternDuration) * _pattern.Length - 1;
-        Light.intensity = _intensityCurve.Evaluate(_lerp ? time : Mathf.Floor(time)) * _normalBrightness;
+        return property;
     }
 
-    private void Awake()
+    private void DrawPreviewGraph()
     {
-        _timer = Random.Range(0, _patternDuration);
+        GUILayout.Label("Flickering Pattern Preview", EditorStyles.boldLabel);
+        using (var _ = new GUILayout.HorizontalScope(GUILayout.Height(_graphHeight)))
+        {
+            using (var __ = new GUILayout.VerticalScope(GUILayout.Width(40)))
+            {
+                _style.alignment = TextAnchor.MiddleLeft;
+                GUILayout.Label("200%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
+                GUILayout.Label("100%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
+                GUILayout.Label("0%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
+            }
+
+            GUI.enabled = false;
+            EditorGUILayout.CurveField("", IntensityCurve.animationCurveValue, Color.yellow, new Rect(0, 0, Pattern.arraySize - 1, 2), GUILayout.ExpandWidth(true), GUILayout.Height(_graphHeight));
+            if ((Application.isPlaying || PlayingInEditMode.boolValue) && Target.Progress >= 0)
+            {
+                var rect = GUILayoutUtility.GetLastRect();
+                GUI.DrawTexture(new Rect(rect.x + Target.Progress * rect.width, rect.y, 1, rect.height), Red);
+            }
+            GUI.enabled = true;
+        }
     }
 
-    private void OnValidate()
+    private void DrawHeaderInfo()
     {
-        _pattern = RestrictToLowercaseAlphabets(_pattern);
-
-        if (_normalBrightness < 0)
+        GUILayout.BeginHorizontal();
+        using (var scope = new GUILayout.VerticalScope())
         {
-            _normalBrightness = 0;
+            _style.fontStyle = FontStyle.Italic;
+            _style.alignment = TextAnchor.MiddleCenter;
+            GUILayout.Label("Normal Brightness scaled from a to z:", _style);
+            GUILayout.Label("a - 0%, m - 100%, z - 200%", _style);
+            _style.fontStyle = FontStyle.Normal;
+            _style.alignment = TextAnchor.MiddleLeft;
         }
-
-        if (_patternDuration < 0.001f)
-        {
-            _patternDuration = 0.001f;
-        }
-
-        if (_cachedPreset != _preset)
-        {
-            _cachedPreset = _preset;
-            _pattern = LightFlickerUtility.GetPatterFromPreset(_preset, _pattern);
-            _cachedPattern = _pattern;
-            RecalculateIntensityCurve();
-        }
-
-        if (!_cachedPattern.Equals(_pattern))
-        {
-            _cachedPattern = _pattern;
-            _preset = LightFlickerUtility.Preset.Custom;
-            _cachedPreset = _preset;
-            RecalculateIntensityCurve();
-        }
-
-        Light.intensity = _normalBrightness;
+        GUILayout.EndHorizontal();
     }
 
-    private void RecalculateIntensityCurve()
+    private void DrawProperties()
     {
-        var keyframes = new List<Keyframe>();
-        for (int i = 0; i < _pattern.Length; i++)
+        DropOptionalButton();
+
+        GUILayout.Space(10);
+        GUILayout.Label("Flicker Pattern", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(Pattern);
+        EditorGUILayout.PropertyField(Preset);
+        EditorGUILayout.PropertyField(NormalBrightness);
+        EditorGUILayout.HelpBox("Normal Brightness will override the Light component's Intensity value.", MessageType.Info);
+
+        GUILayout.Space(10);
+        GUILayout.Label("Speed Settings", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(PatternDuration);
+        EditorGUILayout.PropertyField(Lerp);
+    }
+
+    private void DropOptionalButton()
+    {
+        if (EditorApplication.isPlaying)
         {
-            keyframes.Add
-            (
-                new Keyframe
+            GUI.enabled = false;
+            GUILayout.Button("Preview Button Unavailable in Play Mode");
+            GUI.enabled = true;
+        }
+        else
+        {
+            if (PlayingInEditMode.boolValue)
+            {
+                var color = GUI.backgroundColor;
+                GUI.backgroundColor = Color.red;
+                if (GUILayout.Button($"{'\u25aa'} Stop Preview"))
                 {
-                    time = i,
-                    value = (_pattern[i] - 'a') / 12.5f,
-                    inTangent = 0,
-                    outTangent = 0,
+                    PlayingInEditMode.boolValue = false;
                 }
-            );
+                GUI.backgroundColor = color;
+            }
+            else
+            {
+                var color = GUI.backgroundColor;
+                GUI.backgroundColor = Color.green;
+                if (GUILayout.Button($"{'\u25b8'} Play Preview"))
+                {
+                    PlayingInEditMode.boolValue = true;
+                }
+                GUI.backgroundColor = color;
+            }
         }
-        
-        // "Magic Keyframe" for array size = 1
-        if (_pattern.Length <= 1)
-        {
-            keyframes.Add(new Keyframe { time = 1, value = keyframes[0].value });
-        }
-        _intensityCurve.keys = keyframes.ToArray();
-        _intensityCurve.preWrapMode = WrapMode.Loop;
-        _intensityCurve.postWrapMode = WrapMode.Loop;
     }
-
-    private void Reset()
-    {
-        Light.intensity = _normalBrightness;
-        _playingInEditMode = false;
-    }
-
-#if UNITY_EDITOR
-    private void OnEnable()
-    {
-        UnityEditor.EditorApplication.update += Update;
-        Reset();
-    }
-
-    private void OnDisable()
-    {
-        UnityEditor.EditorApplication.update -= Update;
-        Reset();
-    }
-#endif
-
 }
