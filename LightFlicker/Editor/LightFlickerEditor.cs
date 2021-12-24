249f255d81bd604920db02f6f83966f64702dd4f
huchi57 <felixhu0312@gmail.com>
2021-12-25 01:01:34 +0800

    Update LightFlickerEditor.cs

--- LightFlicker/Editor/LightFlickerEditor.cs
+++ LightFlicker/Editor/LightFlickerEditor.cs
@@ -13,19 +13,39 @@ public class LightFlickerEditor : Editor
     private SerializedProperty _patternDuration = default;
     private SerializedProperty _lerp = default;
     private SerializedProperty _lerpSpeed = default;
-    
+    private SerializedProperty _playingInEditMode = default;
 
+    private static Texture2D _red = null;
     private GUIStyle _cacheStyle = default;
     private GUIStyle _style = default;
     private AnimationCurve _patternCurve = default;
+    private bool _isPlayingInEditMode = false;
 
+    private LightFlicker Target => (LightFlicker) target;
     private SerializedProperty Pattern { get => GetPropertyByName(_pattern, nameof(_pattern)); }
     private SerializedProperty Preset { get => GetPropertyByName(_preset, nameof(_preset)); }
     private SerializedProperty NormalBrightness { get => GetPropertyByName(_normalBrightness, nameof(_normalBrightness)); }
     private SerializedProperty PatternDuration { get => GetPropertyByName(_patternDuration, nameof(_patternDuration)); }
     private SerializedProperty Lerp { get => GetPropertyByName(_lerp, nameof(_lerp)); }
     private SerializedProperty LerpSpeed { get => GetPropertyByName(_lerpSpeed, nameof(_lerpSpeed)); }
-    
+    private SerializedProperty PlayingInEditMode { get => GetPropertyByName(_playingInEditMode, nameof(_playingInEditMode)); }
+    private GUIContent PlayIcon => EditorGUIUtility.IconContent("Animation.Play");
+    private GUIContent PauseIcon => EditorGUIUtility.IconContent("d_PauseButton");
+
+    private static Texture2D Red
+    {
+        get
+        {
+            if (_red == null)
+            {
+                _red = new Texture2D(1, 1);
+                _red.SetPixel(0, 0, Color.red);
+                _red.Apply();
+            }
+            return _red;
+        }
+    }
+
     private AnimationCurve PatternCurve 
     {
         get
@@ -42,6 +62,7 @@ public class LightFlickerEditor : Editor
 
     public override void OnInspectorGUI()
     {
+        Repaint();
         serializedObject.Update();
         _cacheStyle = GUI.skin.label;
         _style = _cacheStyle;
@@ -92,9 +113,9 @@ public class LightFlickerEditor : Editor
     private void DrawPreviewGraph()
     {
         GUILayout.Label("Flickering Pattern Preview", EditorStyles.boldLabel);
-        using (var scope = new GUILayout.HorizontalScope(GUILayout.Height(_graphHeight)))
+        using (var _ = new GUILayout.HorizontalScope(GUILayout.Height(_graphHeight)))
         {
-            using (var scope2 = new GUILayout.VerticalScope(GUILayout.Width(40)))
+            using (var __ = new GUILayout.VerticalScope(GUILayout.Width(40)))
             {
                 _style.alignment = TextAnchor.MiddleLeft;
                 GUILayout.Label("200%", GUILayout.Width(40), GUILayout.Height(_graphHeight / 3));
@@ -105,6 +126,11 @@ public class LightFlickerEditor : Editor
             GUI.enabled = false;
             PatternCurve.keys = GetKeyframes();
             EditorGUILayout.CurveField("", PatternCurve, Color.yellow, new Rect(0, 0, Pattern.arraySize - 1, 2), GUILayout.ExpandWidth(true), GUILayout.Height(_graphHeight));
+            if ((Application.isPlaying || PlayingInEditMode.boolValue) && Target.Progress >= 0)
+            {
+                var rect = GUILayoutUtility.GetLastRect();
+                GUI.DrawTexture(new Rect(rect.x + Target.Progress * rect.width, rect.y, 1, rect.height), Red);
+            }
             GUI.enabled = true;
         }
     }
@@ -116,7 +142,7 @@ public class LightFlickerEditor : Editor
         {
             _style.fontStyle = FontStyle.Italic;
             _style.alignment = TextAnchor.MiddleCenter;
-            GUILayout.Label("Normal Brightness scale from a to z:", _style);
+            GUILayout.Label("Normal Brightness scaled from a to z:", _style);
             GUILayout.Label("a - 0%, m - 100%, z - 200%", _style);
             _style.fontStyle = FontStyle.Normal;
             _style.alignment = TextAnchor.MiddleLeft;
@@ -126,10 +152,42 @@ public class LightFlickerEditor : Editor
 
     private void DrawProperties()
     {
+        if (EditorApplication.isPlaying)
+        {
+            GUI.enabled = false;
+            GUILayout.Button("Preview Button Unavailable in Play Mode");
+            GUI.enabled = true;
+        }
+        else
+        {
+            if (PlayingInEditMode.boolValue)
+            {
+                var color = GUI.backgroundColor;
+                GUI.backgroundColor = Color.red;
+                if (GUILayout.Button($"{'\u25aa'} Stop Preview"))
+                {
+                    PlayingInEditMode.boolValue = false;
+                }
+                GUI.backgroundColor = color;
+            }
+            else
+            {
+                var color = GUI.backgroundColor;
+                GUI.backgroundColor = Color.green;
+                if (GUILayout.Button($"{'\u25b8'} Play Preview"))
+                {
+                    PlayingInEditMode.boolValue = true;
+                }
+                GUI.backgroundColor = color;
+            }
+        }
+
+        GUILayout.Space(10);
         GUILayout.Label("Flicker Pattern", EditorStyles.boldLabel);
         EditorGUILayout.PropertyField(Pattern);
         EditorGUILayout.PropertyField(Preset);
         EditorGUILayout.PropertyField(NormalBrightness);
+        EditorGUILayout.HelpBox("Normal Brightness will override the Light component's Intensity value.", MessageType.Info);
 
         GUILayout.Space(10);
         GUILayout.Label("Speed Settings", EditorStyles.boldLabel);
